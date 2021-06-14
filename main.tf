80873be4bc94fd19cb12326db1f02f1b98420815
Stefan <stefanholmmadsen@gmail.com>
2021-06-14 16:25:55 +0200

    init

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-west-1"
+ 
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-03fc9847ce6863f60"
+  instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
