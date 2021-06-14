abfb2dd3ef7f3ad170b039fd073fa97c75a4b5f5
Stefan <stefanholmmadsen@gmail.com>
2021-06-14 16:31:36 +0200

    init2

--- main.tf
+++ main.tf
@@ -1,17 +1,12 @@
 provider "aws" {
   region = "eu-west-1"
- 
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-03fc9847ce6863f60"
+  ami           = "ami-0ac43988dfd31ab9a"
   instance_type = "t2.micro"
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+
   tags = {
-    Name = "terraform-example"
+    Name = "terraform-poc-by-shm"
   }
 }
